754a36f54351bdddc540af119bde7cdcbf073022
Umut Ugur Uruk <urukumutugur@gmail.com>
2023-04-04 00:27:30 +0300

    commiting

--- article/admin.py
+++ article/admin.py
@@ -3,4 +3,14 @@ from .models import Article
 
 # Register your models here.
 # Oluşturmuş olduğumuz modelleri burada admin paneline belli fonksiyonlar aracılığıyla kayıt ediyoruz.
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'author', 'created_date'] # Article başlığı, article yazarı ve oluşturulma tarihi gösterildi.
+    list_display_links = ['title', 'created_date'] # Başlık ve oluşturulma tarihine tıklayarak bir sonraki sayfaya geçebiliyoruz.
+    search_fields = ['title'] # başlık ve yazar bilgisine göre arama özelliği kazandırdık.
+    list_filter = ['created_date'] # Oluşturulma tarihi filtresine göre seçtiğimiz tarihte oluşturulan makaleleri seçebileceğiz.
+
+    # Bu bize djangonun söylediği bir şey.
+    class Meta:
+        model = Article
--- article/models.py
+++ article/models.py
@@ -4,12 +4,14 @@ from django.db import models
 
 # Article modelini, models içindeki Model class'ından türetiyoruz.
 class Article(models.Model):
-    author = models.ForeignKey("auth.User", on_delete = models.CASCADE) # auth.User tablosundan bir foreign key belirledik(tabloları ilişkilendirdik.) 
+    author = models.ForeignKey("auth.User", on_delete = models.CASCADE, verbose_name = 'Yazar') # auth.User tablosundan bir foreign key belirledik(tabloları ilişkilendirdik.) 
     #on_delete parametresi ile kullanıcı silindiğinde ona ait makalelerin de silinmesine dair belirleme yaptık.
-    title = models.CharField(max_length = 50) # başlık alanını uzunluğu maksimum 50 karakter olacak şekilde oluşturduk.
-    content = models.TextField() # içerik alanını oluşturduk.
-    created_date = models.DateTimeField(auto_now_add = True) # makalenin oluşturulma tarihini o anın tarihine göre atayacak şekilde oluşturduk.
+    title = models.CharField(max_length = 50, verbose_name = 'Başlik') # başlık alanını uzunluğu maksimum 50 karakter olacak şekilde oluşturduk.
+    content = models.TextField(verbose_name = 'İçerik') # içerik alanını oluşturduk.
+    created_date = models.DateTimeField(auto_now_add = True, verbose_name = 'Oluşturulma Tarihi') # makalenin oluşturulma tarihini o anın tarihine göre atayacak şekilde oluşturduk.
+    # article object yerine, o makalenin başlığı gözükücek.
+    def __str__(self):
+        return self.title
     
-
 
 
