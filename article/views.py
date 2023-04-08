3dd6b303563c697f30993d348a0f0e6672e3b582
Umut Ugur Uruk <urukumutugur@gmail.com>
2023-04-08 23:27:46 +0300

    8 Nisan Güncellemesi

--- article/views.py
+++ article/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .forms import ArticleForm
 from .models import Article
 from django.contrib import messages
 
 # Create your views here.
+
+def articles(request):
+    articles = Article.objects.all()
+
+    return render(request, "articles.html", {"articles": articles})
+
 def index(request):
     return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
 
+@login_required(login_url = "user:login")
 def dashboard(request):
     articles = Article.objects.filter(author = request.user)
     context = {
@@ -17,6 +25,7 @@ def dashboard(request):
     }
     return render(request, 'dashboard.html', context)
 
+@login_required(login_url = "user:login")
 def addArticle(request):
     form = ArticleForm(request.POST or None, request.FILES or None)
 
@@ -29,7 +38,28 @@ def addArticle(request):
 
     return render(request, 'addarticle.html', {"form": form})
 
+
 def detail(request, id):
     article = get_object_or_404(Article, id = id)
     return render(request, "detail.html", {"article": article})
     
+@login_required(login_url = "user:login")
+def updateArticle(request, id):
+    article = get_object_or_404(Article, id = id)
+    form = ArticleForm(request.POST or None, request.FILES or None, instance = article)
+    if form.is_valid():
+        article = form.save(commit = False)
+        article.author = request.user
+        article.save()
+        messages.success(request, "Makale başarıyla güncellendi!")
+        return redirect('article:dashboard')
+
+
+    return render(request, "update.html", {"form": form})
+
+@login_required(login_url = "user:login")
+def deleteArticle(request, id):
+    article = get_object_or_404(Article, id = id)
+    article.delete()
+    messages.success(request, "Makale başarıyla silindi")
+    return redirect("article:dashboard")
